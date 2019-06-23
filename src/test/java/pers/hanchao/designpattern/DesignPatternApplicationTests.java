c6ec56a925b25477be8d9ba1924ebf7e7f81308b
hanchao <hanchao@yidian-inc.com>
2019-06-23 16:29:44 +0800

    add:增加log4j2配置

--- src/test/java/pers/hanchao/designpattern/DesignPatternApplicationTests.java
+++ src/test/java/pers/hanchao/designpattern/DesignPatternApplicationTests.java
@@ -1,16 +1,18 @@
 package pers.hanchao.designpattern;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DesignPatternApplicationTests {
 
     @Test
     public void contextLoads() {
+        log.info("Hello Design Pattern !");
     }
-
 }
