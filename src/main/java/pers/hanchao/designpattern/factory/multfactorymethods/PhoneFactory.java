90c2d6ab0c013a9a0d9ac2e803bb1d6ba52b1cd7
hanchao <hanchao@yidian-inc.com>
2019-06-30 14:41:59 +0800

    add:工厂模式:(多个)工厂方法模式

---
+++ src/main/java/pers/hanchao/designpattern/factory/multfactorymethods/PhoneFactory.java
@@ -0,0 +1,29 @@
+package pers.hanchao.designpattern.factory.multfactorymethods;
+
+
+import pers.hanchao.designpattern.factory.simple.telephone.Phone;
+import pers.hanchao.designpattern.factory.simple.telephone.ApplePhone;
+import pers.hanchao.designpattern.factory.simple.telephone.AndroidPhone;
+
+/**
+ * <p>多个工厂方法模式：手机工厂</P>
+ *
+ * @author hanchao
+ */
+public class PhoneFactory {
+
+    /**
+     * 生产一个苹果手机
+     */
+    public static Phone produceApplePhone() {
+        return new ApplePhone();
+    }
+
+    /**
+     * 生产一个安卓手机
+     */
+    public static Phone produceAndroidPhone() {
+        return new AndroidPhone();
+    }
+
+}
